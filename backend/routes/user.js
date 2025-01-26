917a8627a04d5524b573bda0f37df48675ce9d9d
Monishparameswaran <monishparameshwaran@gmail.com>
2025-01-26 19:27:54 +0530

    added user route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -12,19 +12,24 @@ const signupBody = zod.object({
     username: zod.string().email(),
 	firstName: zod.string(),
 	lastName: zod.string(),
-	password: zod.string()
+	password: zod.string().min(6)
 })
 
 router.post("/signup", async (req, res) => {
     const { success } = signupBody.safeParse(req.body)
+   // console.log(req.body.username);
     if (!success) {
         return res.status(411).json({
             message: "Email already taken / Incorrect inputs"
         })
     }
-
+        console.log(req.body.username);
+        console.log(req.body.password);
+        console.log(req.body.firstName);
     const existingUser = await User.findOne({
-        username: req.body.username
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
     })
 
     if (existingUser) {
@@ -50,23 +55,27 @@ router.post("/signup", async (req, res) => {
         userId
     }, JWT_SECRET);
 
-    res.json({
+    res.status(200).json({
         message: "User created successfully",
-        token: token
+        token: token,
+        userId: user._id
+
     })
 })
 
 
 const signinBody = zod.object({
-    username: zod.string().email(),
-	password: zod.string()
+    username: zod.string().email()
 })
 
 router.post("/signin", async (req, res) => {
-    const { success } = signinBody.safeParse(req.body)
+    
+    let name = req.body.username;
+    let password = req.body.password;
+    const { success } = signinBody.safeParse(req.body);
     if (!success) {
         return res.status(411).json({
-            message: "Email already taken / Incorrect inputs"
+            message: "Incorrect inputs"
         })
     }
 
@@ -81,14 +90,15 @@ router.post("/signin", async (req, res) => {
         }, JWT_SECRET);
   
         res.json({
-            token: token
+            token: token,
+            userId: user._id
         })
         return;
     }
 
     
     res.status(411).json({
-        message: "Error while logging in"
+        message: "Error while logging in,No user found"
     })
 })
 
@@ -115,7 +125,7 @@ router.put("/", authMiddleware, async (req, res) => {
     })
 })
 
-router.get("/bulk", async (req, res) => {
+router.get("/bulk",authMiddleware, async (req, res) => {
     const filter = req.query.filter || "";
 
     const users = await User.find({
