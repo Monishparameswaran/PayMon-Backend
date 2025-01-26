2f95eb45e5e44ced5cd2a4bf4ba2542a000ec448
Monishparameswaran <monishparameshwaran@gmail.com>
2025-01-26 19:26:38 +0530

    included logic of express server and added routes

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,15 @@ const rootRouter = require("./routes/index");
 const app = express();
 
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
 
+console.log("Server started at 3000");
 app.listen(3000);
